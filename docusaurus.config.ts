02783f163604d8979e875da89e85049a3047089e
Pipe-Runner <piperunner.in@gmail.com>
2025-04-21 00:23:54 +0200

    Fixed url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://lab.piperunner.in/dotfiles",
+  url: "https://lab.piperunner.in",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
