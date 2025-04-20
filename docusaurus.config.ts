dc2dfc43ee322844e95b4255abfff5901b0b655a
Pipe-Runner <piperunner.in@gmail.com>
2025-04-20 23:54:56 +0200

    Fixed base path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://dotfiles.piperunner.in",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/dotfiles/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
